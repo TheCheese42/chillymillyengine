d661ff7ec95c9ef6d00297cf66dc4416bdd38f3f
NotYou404 <dominik.reinartz@hotmail.com>
2023-07-31 18:29:57 +0200

    testing

--- cme/__init__.py
+++ cme/__init__.py
@@ -18,10 +18,11 @@ def init_cme(
 
 
 # Exports
-from arcade import color, csscolor, gl, key  # noqa
+#from arcade import #color, 
+from arcade import csscolor, gl, key  # noqa
 
 __all__ = [
-    "color",
+    #"color",
     "csscolor",
     "gl",
     "key",
