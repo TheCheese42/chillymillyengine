0563e186c1a1956eb06bd469dfffea5fbaf416cd
Dominik <dominik.reinartz@proton.me>
2024-03-26 19:13:24 +0100

    Added pathfinding

---
+++ cme/pathfinding/__init__.py
@@ -0,0 +1,8 @@
+from arcade.paths import (AStarBarrierList, astar_calculate_path,
+                          has_line_of_sight)
+
+__all__ = [
+    "AStarBarrierList",
+    "astar_calculate_path",
+    "has_line_of_sight",
+]
