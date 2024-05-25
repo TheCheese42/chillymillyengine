aea261767d0b1590b03738194fa7e6b3cad394b6
NotYou404 <dominik.reinartz@hotmail.com>
2024-05-25 18:32:12 +0200

    Changed test font for test_get_optimal_font_size from comic sans ms to arial, as the former seems to not be available on some systems

--- tests/test_utils/test_utils.py
+++ tests/test_utils/test_utils.py
@@ -8,12 +8,12 @@ def test_get_optimal_font_size(initialize_window: None) -> None:
     input_args = [
         ("Hello World!", "Times New Roman", 64, 96),
         ("This is part of our amazing test suite", "Calibri", 12345, 67890),
-        ("Sage, the life you give... Have you ever wondered where it's taken from?", "Comic Sans MS", 1000, 200),  # noqa
+        ("Sage, the life you give... Have you ever wondered where it's taken from?", "Arial", 1000, 200),  # noqa
     ]
     expected_output = [
         9,
         512,
-        29,
+        23,
     ]
     for (
         (text, font_name, container_width, container_height), expected
