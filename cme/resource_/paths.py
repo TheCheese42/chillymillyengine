89ad6d8b9eec6c53517f2dce1ce2e7a48e5456df
Dominik <dominik.reinartz@proton.me>
2024-10-14 14:40:19 +0200

    When creating data and config path, parent dirs will now also be created if necessary

--- cme/resource_/paths.py
+++ cme/resource_/paths.py
@@ -15,10 +15,10 @@ except ImportError:
 
 # Appdata and settings path
 DATA_PATH = Path(resource.get_data_path(app_name))
-DATA_PATH.mkdir(exist_ok=True)
+DATA_PATH.mkdir(parents=True, exist_ok=True)
 SETTINGS_PATH = Path(resource.get_settings_path(app_name))
-SETTINGS_PATH.mkdir(exist_ok=True)
+SETTINGS_PATH.mkdir(parents=True, exist_ok=True)
 
 # Logs
 LOGS_PATH = DATA_PATH / "logs"
-LOGS_PATH.mkdir(exist_ok=True)
+LOGS_PATH.mkdir(parents=True, exist_ok=True)
